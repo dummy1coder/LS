8f347b4fc644c457299c18e2ed8d04b8abb57148
hnafula99 <hannahnyongesa@gmail.com>
2025-04-09 08:17:29 +0300

    Updates

--- resources/views/Frontend/home.blade.php
+++ resources/views/Frontend/home.blade.php
@@ -411,19 +411,8 @@
     </div>
     <!-- Testimonial End -->
 
-    <!-- Vendor Start -->
-    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
-        <div class="container py-5 mb-5">
-            <div class="bg-white">
-                <div class="owl-carousel vendor-carousel">
-                    <img src="img/Onifu Farm Management Logo.png" alt="">
-                    <img src="img/ProEdge CRM.jpg" alt="">
-                    <img src="img/Toppos - Logo.png" alt="">
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Vendor End -->
+@include('includes.vendor')
+
 @include('includes.whatsapp')
 
 @include('includes.footer')
