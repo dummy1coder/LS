7da44150fd33a4407078b396e714eee8e13ba475
Mwaniki David <kilunimwaniki@gmail.com>
2025-04-10 11:18:53 +0300

    updates

--- resources/views/includes/carousel.blade.php
+++ resources/views/includes/carousel.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid position-relative p-0">
     <nav class="navbar navbar-expand-lg navbar-dark px-5 py-3 py-lg-0">
         <a href="{{ route('home') }}" class="navbar-brand p-0">
-            <h1 class="m-0 fs-2"><img src="img/logo.png" alt="Company Logo" width="50" height="50">Learnsoft Beliotech Solutions</h1>
+            <h1 class="m-0 fs-4"><img src="img/logo.png" alt="Company Logo" width="50" height="50">Learnsoft Beliotech Solutions</h1>
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
             <span class="fa fa-bars"></span>
@@ -36,7 +36,7 @@
                 <div class="carousel-caption d-flex flex-row align-items-center justify-content-end">
                     <div class="mx-auto" style="width: 1000px;">
                         <h3 class="text-white text-uppercase mb-3 animated slideInDown"><b>Revolutionize with Our Game-Changing Solutions</b></h3>
-                        <h2 class="display-8 text-white mb-md-4 animated zoomIn">Embark on a journey of innovation with our state of the art software solutions. Tested and proven, our cutting-edge products set new standards for excellence. Acclaimed for their brilliance, our award-winning services bring authenticity and reliability to the forefront. Experience the power of advanced technology that not only excels but is also within reach.</h2>
+                        <h4 class="display-8 text-white mb-md-4 animated zoomIn">Embark on a journey of innovation with our state of the art software solutions. Tested and proven, our cutting-edge products set new standards for excellence. Acclaimed for their brilliance, our award-winning services bring authenticity and reliability to the forefront. Experience the power of advanced technology that not only excels but is also within reach.</h4>
                     </div>
                 </div>
             </div>
@@ -45,7 +45,7 @@
                 <div class="carousel-caption d-flex flex-row align-items-center justify-content-end">
                     <div class="mx-auto" style="width: 1000px;">
                     <h3 class="text-white text-uppercase mb-3 animated slideInDown"><b>Empower Progress with Our Innovative Software Tools</b></h3>
-                        <h2 class="display-8 text-white mb-md-4 animated zoomIn">Ignite innovation with our pioneering software platforms. With a proven track record of success, our tools are crafted to lead and inspire. Recognized for outstanding achievement, we bring clarity, confidence, and efficiency to every project. Access the future engineered for now.</h2>
+                        <h4 class="display-8 text-white mb-md-4 animated zoomIn">Ignite innovation with our pioneering software platforms. With a proven track record of success, our tools are crafted to lead and inspire. Recognized for outstanding achievement, we bring clarity, confidence, and efficiency to every project. Access the future engineered for now.</h4>
                     </div>
                 </div>
             </div>
@@ -54,7 +54,7 @@
                 <div class="carousel-caption d-flex flex-row align-items-center justify-content-end">
                     <div class="mx-auto" style="width: 1000px;">
                     <h3 class="text-white text-uppercase mb-3 animated slideInDown"><b>Disrupt the Norm with Our Next-Level Solutions</b></h3>
-                        <h2 class="display-8 text-white mb-md-4 animated zoomIn">Fuel your growth with transformative software designed for modern challenges. Validated by results and trusted by professionals, our solutions are more than tools, they’re a new standard. Renowned for excellence, our services deliver both strength and simplicity. The future of tech is here and it’s ready for you.</h2>
+                        <h4 class="display-8 text-white mb-md-4 animated zoomIn">Fuel your growth with transformative software designed for modern challenges. Validated by results and trusted by professionals, our solutions are more than tools, they’re a new standard. Renowned for excellence, our services deliver both strength and simplicity. The future of tech is here and it’s ready for you.</h4>
                     </div>
                 </div>
             </div>
@@ -63,7 +63,7 @@
                 <div class="carousel-caption d-flex flex-row align-items-center justify-content-end">
                     <div class="mx-auto" style="width: 1000px;">
                     <h3 class="text-white text-uppercase mb-3 animated slideInDown"><b>Lead the Change with Our Cutting-Edge Technology</b></h3>
-                        <h2 class="display-8 text-white mb-md-4 animated zoomIn">Unleash potential with cutting-edge digital solutions engineered for excellence. Rigorously tested and widely trusted, our technology sets benchmarks in performance and quality. Honored across the industry, our services embody precision and dependability. Embrace a smarter tomorrow made accessible today.</h2>
+                        <h4 class="display-8 text-white mb-md-4 animated zoomIn">Unleash potential with cutting-edge digital solutions engineered for excellence. Rigorously tested and widely trusted, our technology sets benchmarks in performance and quality. Honored across the industry, our services embody precision and dependability. Embrace a smarter tomorrow made accessible today.</h4>
                     </div>
                 </div>
             </div>
