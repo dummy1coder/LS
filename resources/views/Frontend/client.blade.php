a643f5a08b204d5c6f2c5dfe2191567dc31c5e29
hnafula99 <hannahnyongesa@gmail.com>
2025-04-10 13:17:02 +0300

    Updates

--- resources/views/Frontend/client.blade.php
+++ resources/views/Frontend/client.blade.php
@@ -44,7 +44,7 @@
                 <div class="col-lg-4 wow slideInUp" data-wow-delay="0.6s">
                     <div class="team-item bg-light rounded overflow-hidden">
                         <div class="team-img position-relative overflow-hidden">
-                            <img class="img-fluid w-100" src="img/embutopmark.jpg" alt="" style="height: 250px; object-fit: cover;">
+                            <img class="img-fluid w-100" src="img/embutopmark.jpg" alt="" style="height: 250px; object-fit: cover; object-position:top">
                         </div>
                         <div class="text-center py-4">
                             <h4 class="text-primary">Embu Top Mark Academy</h4>
@@ -55,11 +55,8 @@
                 <div class="col-lg-4 wow slideInUp" data-wow-delay="0.9s">
                     <div class="team-item bg-light rounded overflow-hidden">
                         <div class="team-img position-relative overflow-hidden">
-<<<<<<< HEAD
                             <img class="img-fluid w-100" src="img/premese.jpg" alt="">
-=======
                             <img class="img-fluid w-100" src="img/premese.jpeg" alt="" style="height: 250px; object-fit: cover;">
->>>>>>> 9cd5c24ac1fbe57a906ea6e1b2c0c7e5755c4a3c
                         </div>
                         <div class="text-center py-4">
                             <h4 class="text-primary">Premese Academy</h4>
