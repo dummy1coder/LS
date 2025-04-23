142c7f797a41375e4685174f195b411a5f38afcd
Mwaniki David <kilunimwaniki@gmail.com>
2025-04-23 21:41:13 +0300

    update

--- resources/views/Frontend/home.blade.php
+++ resources/views/Frontend/home.blade.php
@@ -279,7 +279,7 @@
                     <div class="position-relative bg-primary rounded h-100 d-flex flex-column align-items-center justify-content-center text-center p-5">
                         <h3 class="text-white mb-3">Call Us For Quote and Demo</h3>
                         <p class="text-white mb-3"></p>
-                        <h2 class="text-white mb-0">+254 711 667 919</h2>
+                        <h3 class="text-white mb-0">+254 711 667 919</h3>
                     </div>
                 </div>
             </div>
